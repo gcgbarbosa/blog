6bde3987eff7e941eeebf489461695f100fcad4a
gcgbarbosa <gcgbarbosa@gmail.com>
2025-08-18 15:13:14 +0200

    refactor: migrating to astro

---
+++ gcgbarbosa/src/content.config.ts
@@ -0,0 +1,25 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content';
+
+// 2. Import loader(s)
+import { glob } from 'astro/loaders';
+
+// 3. Define your collection(s)
+const blog = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/posts" }),
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+    tags: z.array(z.string())
+  }),
+});
+
+// 4. Export a single `collections` object to register your collection(s)
+export const collections = { blog };
+
