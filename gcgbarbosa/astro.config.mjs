0a24b6bc1580335492bdca95446dbf0a0bc0b928
gcgbarbosa <gcgbarbosa@gmail.com>
2025-08-18 17:26:50 +0200

    feat: mdx

--- gcgbarbosa/astro.config.mjs
+++ gcgbarbosa/astro.config.mjs
@@ -1,5 +1,30 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+
+import expressiveCode from 'astro-expressive-code';
+
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()], // type: ignore
+  },
+
+  markdown: {
+    shikiConfig: {
+      theme: 'dracula',
+    },
+  },
+
+  integrations: [expressiveCode({
+    themes: ['github-dark', 'github-light'],      // light/dark out of the box
+    defaultProps: { wrap: true },                 // wrap long lines by default (optional)
+    // Make code themes follow your DaisyUI theme (data-theme="dark|light"):
+    customizeTheme(theme) {
+      theme.name = theme.type; // renames to 'dark' or 'light'
+    },
+    // frames: { showCopyToClipboardButton: true } // on by default
+  }), mdx()],
+});
--- gcgbarbosa/src/content.config.ts
+++ gcgbarbosa/src/content.config.ts
@@ -6,7 +6,7 @@ import { glob } from 'astro/loaders';
 
 // 3. Define your collection(s)
 const blog = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "./src/posts" }),
+  loader: glob({ pattern: "**/*.mdx", base: "./src/posts" }),
   schema: z.object({
     title: z.string(),
     pubDate: z.date(),
